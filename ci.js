6df8e97db6549f95033abede411c5b2280bdb102
lu6fer <lu6fer2604@gmail.com>
2020-02-04 09:37:48 +0100

    ci

--- ci.js
+++ ci.js
@@ -1,11 +1,41 @@
 const { Octokit } = require('@octokit/rest');
+const Git = require('nodegit');
+
+
+process.on('unhandledRejection', (err) => {
+  console.error(err)
+  process.exit(1)
+});
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN
 });
 
-octokit.issues.list({
+/*octokit.pulls.list({
+  owner: 'fhunkeler',
+  repo: 'test',
   state: 'open'
-}).then(data => {
-  console.log(data)
-}).catch(err => console.log(err));
+}).then(res => {
+  console.log(res.data.map(pull => console.log(pull.labels)))
+}).catch(err => console.log(err));*/
+
+
+async function listPR() {
+  try {
+    const prLsit = await octokit.pulls.list({
+      owner: 'fhunkeler',
+      repo: 'test',
+      state: 'open'
+    });
+    return prLsit.data.filter(k => k.labels.some(e => e.name !== 'DO_NOT_INTEGRATE'));
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+async function run() {
+  console.log(await listPR());
+}
+
+run();
